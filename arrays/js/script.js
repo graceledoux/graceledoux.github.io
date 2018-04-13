2d7cbdf501345900a377a2be22ec4c369491c988
Grace Ledoux <ledog438@newschool.edu>
2018-04-13 09:17:22 -0400

    Revert "added 4.13 post"
    This reverts commit d05d7a195cc8083a331a84150a7cedefad10b866.

--- arrays/js/script.js
+++ arrays/js/script.js
@@ -7,15 +7,12 @@ $("#submit").on("click", function(){
     if(list_item == items[i]){
       alert("That's already on the list!"); break;
     }
-
   else{
-
   items.push(list_item);
   var output = $("#output");
   var temp = "";
   for(var i = 0; i < items.length; i++){
     temp = temp + "<p>" + items[i] + "</p>"}
-  output.html (temp);
-}
+  output.html (temp);}
 
 }});
